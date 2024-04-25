cc24dee48b17c1a3400065f6dbc6dfd3a4b3cd4f
Mir2812 <comingwolf12@gmail.com>
2024-04-25 00:30:43 -0700

    verse

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -86,7 +86,7 @@ async def bot_send_picture(message:types.Message):
 
 @dp.message_handler(text="Фрукты и Кофе")
 async def bot_send_picture(message:types.Message):
-    for nomer in range(660,784):
+    for nomer in range(660,748):
         photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
         await message.answer_photo(photo=photo_url)
 
