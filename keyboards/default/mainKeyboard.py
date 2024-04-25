ebc032dfea79426f9c0730e37392778ac0782fe3
jsm020 <javohirxikmatillaev@gmail.com>
2024-04-25 09:46:11 +0500

    1.1

---
+++ keyboards/default/mainKeyboard.py
@@ -0,0 +1,23 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+menuPython = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Вода"),
+            KeyboardButton(text="Водопад"),
+        ],
+        [
+            KeyboardButton(text="Город"),
+            KeyboardButton(text="Детское"),
+        ],
+        [
+            KeyboardButton(text="Космос&Луна"),
+            KeyboardButton(text="Небо"),
+        ],
+        [
+            KeyboardButton(text="Дельфин"),
+            KeyboardButton(text="Небо & Птицы"),
+        ],
+    ],
+    resize_keyboard=True
+)
