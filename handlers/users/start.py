a0ff6db3b28ee5eda705784b37002b55ef03e2f8
Mir2812 <comingwolf12@gmail.com>
2024-04-25 00:05:59 -0700

    list_qo'shildi

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -11,7 +11,7 @@ async def bot_start(message: types.Message):
 
 
 
-@dp.message_handler(text="Вода")
+@dp.message_handler(text="Абстракция и 3d")
 async def bot_send_picture(message:types.Message):
     for nomer in range(2,84):
         photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
@@ -34,3 +34,47 @@ async def bot_send_picture(message:types.Message):
     for nomer in range(309,442):
         photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
         await message.answer_photo(photo=photo_url)
+
+@dp.message_handler(text="Космос & Луна")
+async def bot_send_picture(message:types.Message):
+    for nomer in range(443,591):
+        photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
+        await message.answer_photo(photo=photo_url)
+
+@dp.message_handler(text="Небо")
+async def bot_send_picture(message:types.Message):
+    for nomer in range(592,659):
+        photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
+        await message.answer_photo(photo=photo_url)
+
+
+@dp.message_handler(text="Фрукты и Кофе")
+async def bot_send_picture(message:types.Message):
+    for nomer in range(660,659):
+        photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
+        await message.answer_photo(photo=photo_url)
+
+@dp.message_handler(text="Птицы")
+async def bot_send_picture(message:types.Message):
+    for nomer in range(592,659):
+        photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
+        await message.answer_photo(photo=photo_url)
+
+@dp.message_handler(text="Природа")
+async def bot_send_picture(message:types.Message):
+    for nomer in range(592,659):
+        photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
+        await message.answer_photo(photo=photo_url)
+
+@dp.message_handler(text="Исторически")
+async def bot_send_picture(message:types.Message):
+    for nomer in range(592,659):
+        photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
+        await message.answer_photo(photo=photo_url)
+
+@dp.message_handler(text="ультрафеле")
+async def bot_send_picture(message:types.Message):
+    for nomer in range(592,659):
+        photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
+        await message.answer_photo(photo=photo_url)
+
