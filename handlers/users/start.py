bd756930e09cc691bdbd71d8c46ccf72ad11a0f1
jsm020 <javohirxikmatillaev@gmail.com>
2024-04-25 13:14:52 +0500

    8

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -12,52 +12,75 @@ async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!",reply_markup=menuPython)
 
 
-list = [2,22,34,46,58,60,72,84]
+list_abs = [2,22,34,46,58,60,72,84]
 async def send_photos_range(message: types.Message, start_index: int, end_index: int, next_command: str):
-    for nomer in range(list[start_index], list[end_index]):
+    for nomer in range(list_abs[start_index], list_abs[end_index]):
         photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
         await message.answer_photo(photo=photo_url)
     await message.answer(f"Keyingisi uchun /{next_command}")
 
 async def send_last_photos_range(message: types.Message, start_index: int, end_index: int):
-    for nomer in range(list[start_index], list[end_index]):
+    for nomer in range(list_abs[start_index], list_abs[end_index]):
         photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
         await message.answer_photo(photo=photo_url)
 
-@dp.message_handler(text="Абстракция и 3d", state="*")
+@dp.message_handler(text="Абстракция & 3d", state="*")
 async def bot_send_picture(message: types.Message):
-    await send_photos_range(message, 0, 1, "next_page_2_voda")
+    await send_photos_range(message, 0, 1, "next_page_2_abs")
 
-@dp.message_handler(commands='next_page_2_voda')
+@dp.message_handler(commands='next_page_2_abs')
 async def bot_send_picture_2(message: types.Message):
-    await send_photos_range(message, 1, 2, "next_page_3_voda")
+    await send_photos_range(message, 1, 2, "next_page_3_abs")
 
-@dp.message_handler(commands='next_page_3_voda')
+@dp.message_handler(commands='next_page_3_abs')
 async def bot_send_picture_3(message: types.Message):
-    await send_photos_range(message, 2, 3, "next_page_4_voda")
+    await send_photos_range(message, 2, 3, "next_page_4_abs")
 
-@dp.message_handler(commands='next_page_4_voda')
+@dp.message_handler(commands='next_page_4_abs')
 async def bot_send_picture_4(message: types.Message):
-    await send_photos_range(message, 3, 4, "next_page_5_voda")
+    await send_photos_range(message, 3, 4, "next_page_5_abs")
 
-@dp.message_handler(commands='next_page_5_voda')
+@dp.message_handler(commands='next_page_5_abs')
 async def bot_send_picture_5(message: types.Message):
-    await send_photos_range(message, 4, 5, "next_page_6_voda")
+    await send_photos_range(message, 4, 5, "next_page_6_abs")
 
-@dp.message_handler(commands='next_page_6_voda')
+@dp.message_handler(commands='next_page_6_abs')
 async def bot_send_picture_6(message: types.Message):
-    await send_photos_range(message, 5, 6, "next_page_7_voda")
+    await send_photos_range(message, 5, 6, "next_page_7_asb")
 
-@dp.message_handler(commands='next_page_7_voda')
+@dp.message_handler(commands='next_page_7_abs')
 async def bot_send_picture_7(message: types.Message):
     await send_last_photos_range(message, 6, 7)
 
 
-@dp.message_handler(text="Водопад", state="*")
-async def bot_send_picture(message:types.Message):
-    for nomer in range(85,108):
+
+list_vod = [84,90,100,108]
+async def send_photos_range(message: types.Message, start_index: int, end_index: int, next_command: str):
+    for nomer in range(list_vod[start_index], list_vod[end_index]):
         photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
         await message.answer_photo(photo=photo_url)
+    await message.answer(f"Keyingisi uchun /{next_command}")
+
+async def send_last_photos_range(message: types.Message, start_index: int, end_index: int):
+    for nomer in range(list_vod[start_index], list_vod[end_index]):
+        photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
+        await message.answer_photo(photo=photo_url)
+
+@dp.message_handler(text="Водопад", state="*")
+async def bot_send_picture(message: types.Message):
+    await send_photos_range(message, 0, 1, "next_page_2_vod")
+
+@dp.message_handler(commands='next_page_2_vod')
+async def bot_send_picture_2(message: types.Message):
+    await send_photos_range(message, 1, 2, "next_page_3_vod")
+
+@dp.message_handler(commands='next_page_3_vod')
+async def bot_send_picture_3(message: types.Message):
+    await send_last_photos_range(message, 2, 3)
+
+
+
+
 
 @dp.message_handler(text="Город", state="*")
 async def bot_send_picture(message:types.Message):
@@ -86,13 +109,13 @@ async def bot_send_picture(message:types.Message):
 
 @dp.message_handler(text="Фрукты и Кофе")
 async def bot_send_picture(message:types.Message):
-    for nomer in range(660,748):
+    for nomer in range(660,760):
         photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
         await message.answer_photo(photo=photo_url)
 
 @dp.message_handler(text="Птицы")
 async def bot_send_picture(message:types.Message):
-    for nomer in range(760,792):
+    for nomer in range(761,792):
         photo_url = f"https://t.me/asafsfbdbfksdbgosgsbgeiw/{nomer}"
         await message.answer_photo(photo=photo_url)
 
--- keyboards/default/mainKeyboard.py
+++ keyboards/default/mainKeyboard.py
@@ -3,31 +3,32 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 menuPython = ReplyKeyboardMarkup(
     keyboard=[
         [
+            KeyboardButton(text="Абстракция & 3d"),
             KeyboardButton(text="Вода"),
-            KeyboardButton(text="Водопад"),
         ],
         [
+            KeyboardButton(text="Водопад"),
             KeyboardButton(text="Город"),
+        ],
+        [
             KeyboardButton(text="Детское"),
+            KeyboardButton(text="Исторически"),
         ],
         [
             KeyboardButton(text="Космос & Луна"),
             KeyboardButton(text="Небо"),
         ],
         [
-            KeyboardButton(text="Фрукты и Кофе"),
+            KeyboardButton(text="Природа"),
             KeyboardButton(text="Птицы"),
         ],
         [
-            KeyboardButton(text="Природа"),
-            KeyboardButton(text="Абстракция & 3d"),
-        ],
-        [
-            KeyboardButton(text="Исторически"),
+            KeyboardButton(text="Фрукты и Кофе"),
             KeyboardButton(text="ультрафеле"),
         ],
     ],
     resize_keyboard=True
 )
+
 
 
